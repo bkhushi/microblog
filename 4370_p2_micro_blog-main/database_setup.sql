6ad5c38c4f85e97a9b6baa641e812aed78760b1a
Aditi Chiluguri <aditi.chiluguri@gmail.com>
2025-03-20 15:32:38 -0400

    fixed sql file

--- 4370_p2_micro_blog-main/database_setup.sql
+++ 4370_p2_micro_blog-main/database_setup.sql
@@ -23,8 +23,8 @@ CREATE TABLE if not exists follow (
     following_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (follower_id, following_id),
-    FOREIGN KEY (follower_id) REFERENCES user(user_id),
-    FOREIGN KEY (following_id) REFERENCES user(user_id)
+    FOREIGN KEY (follower_id) REFERENCES user(userID),
+    FOREIGN KEY (following_id) REFERENCES user(userID)
 );
 -- Create hashtag table
 CREATE TABLE if not exists hashtag (
