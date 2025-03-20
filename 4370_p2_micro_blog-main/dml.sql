c4b79e7744dd9731eeeb68222b5b13d43b5694a6
Grace <ger85847@uga.edu>
2025-03-19 20:34:24 -0400

    updated dml to have new post tables

--- 4370_p2_micro_blog-main/dml.sql
+++ 4370_p2_micro_blog-main/dml.sql
@@ -1,14 +1,14 @@
 -- Display bookmarked posts, excluding the logged in user, and order by most recent to oldest. Used in the Bookmarks page.
 -- Url:
-SELECT DISTINCT p.*, u.username, u.firstName, u.lastName   
-FROM post p  
-JOIN user u ON p.user_id = u.userId 
-WHERE p.isBookmarked = true AND p.user_id != ? 
-ORDER BY p.created_at DESC;
+SELECT DISTINCT p.*, u.username, u.firstName, u.lastName
+FROM post_no_comments p
+JOIN user u ON p.user_id = u.userId
+WHERE p.is_bookmarked = true AND p.user_id != ?
+ORDER BY p.created_at DESC
 
 -- Display all posts from a specific user and order by most recent to oldest. Used in the Profile page.
 -- Url:
 SELECT * 
-FROM post 
+FROM post_no_comments
 WHERE user_id = ? 
-ORDER BY p.created_at DESC;
+ORDER BY created_at DESC
